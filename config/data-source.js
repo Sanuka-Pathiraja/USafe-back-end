bb9f7f4842a4b7ac36b70d9375c326c61dd094d5
Navi2004725 <naveeshabeydeera123@gmail.com>
2026-01-19 13:32:20 +0530

    commit 03 part 02 ✏️

--- config/data-source.js
+++ config/data-source.js
@@ -1,19 +1,23 @@
-import "dotenv/config";
 import { DataSource } from "typeorm";
+import "dotenv/config";
 
 const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST, // The Host from Supabase
-  port: Number(process.env.DB_PORT), // 6543
-  username: process.env.DB_USER, // postgres.xxxx
-  password: process.env.DB_PASS, // Your password
-  database: process.env.DB_NAME, // postgres
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   synchronize: true,
   logging: true,
   entities: ["./Models/*.js"],
-  // SUPABASE REQUIRES THIS:
+  // Add these for Supabase Pooler support
   ssl: {
     rejectUnauthorized: false,
+  },
+  extra: {
+    max: 20, // Helps manage connections for your 6 members
+    connectionTimeoutMillis: 2000,
   },
 });
 
