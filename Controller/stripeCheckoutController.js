e4d61d4b69d57a86be366d0889a92804cc6b5b1c
Navi2004725 <naveeshabeydeera123@gmail.com>
2026-02-10 17:16:54 +0530

    day 12 finalized stripe to transaction with lkr ✏️

--- Controller/stripeCheckoutController.js
+++ Controller/stripeCheckoutController.js
@@ -7,25 +7,21 @@ export const createCheckoutSession = async (req, res) => {
 
     const session = await stripe.checkout.sessions.create({
       mode: "payment",
-
       payment_method_types: ["card"],
-
       line_items: [
         {
           price_data: {
-            currency: "usd",
+            currency: "lkr", // Fixed: Correct currency
             product_data: {
-              name: "uSafe Premium",
+              name: "uSafe Premium Upgrade",
             },
-            unit_amount: amount * 100,
+            unit_amount: amount * 100, // 160 LKR becomes 16000
           },
           quantity: 1,
         },
       ],
-
       success_url: "http://10.0.2.2:5000/payment/success?session_id={CHECKOUT_SESSION_ID}",
       cancel_url: "http://10.0.2.2:5000/payment/cancel",
-
       metadata: {
         user_id: req.user.id,
       },
