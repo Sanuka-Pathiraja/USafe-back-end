16953194b81de5c3623379da6bac6b37a4408489
vinhara <vinharaperera@gmail.com>
2026-03-08 21:20:15 +0530

    Defining Earth radius and coordinate differences

--- utils/distance.js
+++ utils/distance.js
@@ -1,1 +1,5 @@
-function calculateDistance(lat1, lon1, lat2, lon2) {}
+function calculateDistance(lat1, lon1, lat2, lon2) {
+    const R = 6371000;
+    const dLat = (lat2 - lat1) * Math.PI / 180;
+    const dLon = (lon2 - lon1) * Math.PI / 180;
+}
