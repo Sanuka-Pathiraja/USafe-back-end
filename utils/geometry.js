5efa604e4e9511415dc6f8144f8216174fa48c30
SithumiPalihena <sithumipalihena9@gmail.com>
2026-03-08 21:33:48 +0530

    Start loop to generate points for polygon

--- utils/geometry.js
+++ utils/geometry.js
@@ -3,5 +3,10 @@ function circleToPolygon(centerLon, centerLat, radiusMeters, numPoints = 32) {
     const coords = [];
     const earthRadius = 6378137;
   
+    for (let i = 0; i < numPoints; i++) {
+
+    
+    
+  }
 }
 
