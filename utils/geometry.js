a0912fba3a22099098b6f256bfc2cf0c8d8e70d9
SithumiPalihena <sithumipalihena9@gmail.com>
2026-03-08 21:34:22 +0530

    Calculate angle for current point

--- utils/geometry.js
+++ utils/geometry.js
@@ -5,6 +5,8 @@ function circleToPolygon(centerLon, centerLat, radiusMeters, numPoints = 32) {
   
     for (let i = 0; i < numPoints; i++) {
 
+        const angle = (i / numPoints) * 2 * Math.PI;
+
     
     
   }
