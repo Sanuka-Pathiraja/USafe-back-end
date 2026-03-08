7faacb7aa412273fcfb73676d4f05778f90af82e
SithumiPalihena <sithumipalihena9@gmail.com>
2026-03-08 21:08:21 +0530

    Build Mapbox Directions API URL with start and end coordinates

--- NaviFeat/mapboxService.js
+++ NaviFeat/mapboxService.js
@@ -1,3 +1,8 @@
 const axios = require("axios");
 
 const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN;
+
+const getRoutesFromMapbox = async (start, end) => {
+
+  const url = `https://api.mapbox.com/directions/v5/mapbox/walking/${start.lon},${start.lat};${end.lon},${end.lat}?alternatives=true&geometries=geojson&access_token=${MAPBOX_TOKEN}`;
+};
