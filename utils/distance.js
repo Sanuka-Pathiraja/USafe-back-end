38155a0876438a77633537ca5810dbf111b3b045
vinhara <vinharaperera@gmail.com>
2026-03-08 21:23:54 +0530

    Finalize distance calculation

--- utils/distance.js
+++ utils/distance.js
@@ -8,4 +8,8 @@ function calculateDistance(lat1, lon1, lat2, lon2) {
     Math.cos(lat1 * Math.PI / 180) *
     Math.cos(lat2 * Math.PI / 180) *
     Math.sin(dLon / 2) ** 2;
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c;
+  
 }
