fe20144643d4708d3c184cf47b202b5c08f8a061
SithumiPalihena <sithumipalihena9@gmail.com>
2026-03-08 21:09:18 +0530

    Return the routes from Mapbox response

--- NaviFeat/mapboxService.js
+++ NaviFeat/mapboxService.js
@@ -4,5 +4,8 @@ const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN;
 
 const getRoutesFromMapbox = async (start, end) => {
 
-  const url = `https://api.mapbox.com/directions/v5/mapbox/walking/${start.lon},${start.lat};${end.lon},${end.lat}?alternatives=true&geometries=geojson&access_token=${MAPBOX_TOKEN}`;
+    const url = `https://api.mapbox.com/directions/v5/mapbox/walking/${start.lon},${start.lat};${end.lon},${end.lat}?alternatives=true&geometries=geojson&access_token=${MAPBOX_TOKEN}`;
+    const response = await axios.get(url);
+
+    return response.data.routes;
 };
