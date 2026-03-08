33956128d1d9f4269928582796dfaf810cac6a9b
SithumiPalihena <sithumipalihena9@gmail.com>
2026-03-08 20:45:52 +0530

    Export router module

--- Routers/routeRouter.js
+++ Routers/routeRouter.js
@@ -2,3 +2,4 @@ const express = require("express");
 const router = express.Router();
 const { getSafeRoute } = require("../Controller/routeController");
 router.post("/safe-route", getSafeRoute);
+module.exports = router;
