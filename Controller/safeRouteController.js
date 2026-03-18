9f1d846f77488ac1fe47cf2915e2b28b2d95f246
SithumiPalihena <sithumipalihena9@gmail.com>
2026-03-18 14:13:15 +0530

    Import circleToPolygon utility (updated version)

--- Controller/safeRouteController.js
+++ Controller/safeRouteController.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
-
 const circleToPolygon = require("../utils/circlePolygon");
-const routeIntersectsZones = require("../utils/zoneChecker");
+
+const getSafeRoute = async (req, res) => {};
+module.exports = { getSafeRoute };
