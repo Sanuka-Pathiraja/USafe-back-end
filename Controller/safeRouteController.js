18f542b152eee34628a8e2ae40353ec6c1a946a0
SithumiPalihena <sithumipalihena9@gmail.com>
2026-03-18 14:09:57 +0530

    Initialize getSafeRoute async handler (updated version)

--- Controller/safeRouteController.js
+++ Controller/safeRouteController.js
@@ -1,31 +1,4 @@
 const axios = require("axios");
-const { routeIntersectsZones } = require("../utils/distance");
-const { getRoutesFromMapbox } = require("../NaviFeat/mapboxService");
-const getSafeRoute = async (req, res) => {
-    try {
-      const { start, end, redZones } = req.body;
-  
-      const routes = await getRoutesFromMapbox(start, end);
-  
-      for (let route of routes) {
-        const intersects = routeIntersectsZones(route.geometry.coordinates, redZones);
-  
-        if (!intersects) {
-          return res.json({
-            status: "success",
-            message: "Safe route found",
-            route
-          });
-        }
-      }
-  
-      res.json({
-        status: "warning",
-        message: "No completely safe route found"
-      });
-  
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  };
-  module.exports = { getSafeRoute };
+
+const circleToPolygon = require("../utils/circlePolygon");
+const routeIntersectsZones = require("../utils/zoneChecker");
