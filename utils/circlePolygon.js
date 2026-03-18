343f6f314778f225251c804a27b1392ea8368065
SithumiPalihena <sithumipalihena9@gmail.com>
2026-03-18 15:05:16 +0530

    Add circleToPolygon function with parameters for center, radius, and number of points

--- utils/circlePolygon.js
+++ utils/circlePolygon.js
@@ -1,27 +1,3 @@
 function circleToPolygon(centerLon, centerLat, radiusMeters, numPoints = 32) {
-
-    const coords = [];
-    const earthRadius = 6378137;
-  
-    for (let i = 0; i < numPoints; i++) {
-
-        const angle = (i / numPoints) * 2 * Math.PI;
-        const dx = radiusMeters * Math.cos(angle);
-        const dy = radiusMeters * Math.sin(angle);
-
-        const dLon = dx / (earthRadius * Math.cos(centerLat * Math.PI / 180));
-        const dLat = dy / earthRadius;
     
-        coords.push([
-        centerLon + dLon * 180 / Math.PI,
-        centerLat + dLat * 180 / Math.PI
-        ]);
-
-        coords.push(coords[0]);
-
-        return coords;
-    
-  }
 }
-module.exports = { circleToPolygon };
-
