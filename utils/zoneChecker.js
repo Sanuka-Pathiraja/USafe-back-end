bc348051d9a6f6b8597bca92b3db0dbaffbf7a5b
vinhara <vinharaperera@gmail.com>
2026-03-18 15:22:06 +0530

    adding the fully updated code

--- utils/zoneChecker.js
+++ utils/zoneChecker.js
@@ -1,7 +1,9 @@
 const calculateDistance = require("./distance");
 
 function routeIntersectsZones(routeCoords, redZones) {
+
   for (let coord of routeCoords) {
+
     for (let zone of redZones) {
 
       const distance = calculateDistance(
@@ -15,6 +17,8 @@ function routeIntersectsZones(routeCoords, redZones) {
 
     }
   }
+
+  return false;
 }
 
 module.exports = routeIntersectsZones;
