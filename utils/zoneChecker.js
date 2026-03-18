7a31095711a513ade4182607a4cc8fcfe9d8d066
vinhara <vinharaperera@gmail.com>
2026-03-18 15:15:51 +0530

    implement iteration over route coordinates and zones

--- utils/zoneChecker.js
+++ utils/zoneChecker.js
@@ -1,9 +1,5 @@
 function routeIntersectsZones(routeCoords, redZones) {
-    for (let coord of routeCoords) {
-      for (let zone of redZones) {
-  
-      }
-    }
-  }
-  
-  module.exports = routeIntersectsZones;
+
+}
+
+module.exports = routeIntersectsZones;
