70fe6388f79b7d5496833bc6aca16a1c844ae6ae
SithumiPalihena <sithumipalihena9@gmail.com>
2026-03-18 14:15:06 +0530

    Add hardcoded start coordinates (updated version)

--- Controller/safeRouteController.js
+++ Controller/safeRouteController.js
@@ -4,3 +4,4 @@ const routeIntersectsZones = require("../utils/zoneChecker");
 
 const getSafeRoute = async (req, res) => {};
 module.exports = { getSafeRoute };
+const start = { lat: 6.8391, lon: 79.8817 };
