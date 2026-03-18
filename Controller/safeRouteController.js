85e5019f42a468ddcfede0cb7735c530b2cf8596
SithumiPalihena <sithumipalihena9@gmail.com>
2026-03-18 14:25:23 +0530

    Fetch routes from Mapbox API using axios

--- Controller/safeRouteController.js
+++ Controller/safeRouteController.js
@@ -13,6 +13,14 @@ const getSafeRoute = async (req, res) => {
     ];
 
     const url = `https://api.mapbox.com/directions/v5/mapbox/driving/${start.lon},${start.lat};${end.lon},${end.lat}`;
+
+    const response = await axios.get(url, {
+      params: {
+        geometries: 'geojson',
+        access_token: process.env.MAPBOX_TOKEN,
+        alternatives: true,
+        overview: 'full'
+      } });
     
   
 }
