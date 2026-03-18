5ecd081a7eb733f8b408c6337ced7f2aa2cc4733
SithumiPalihena <sithumipalihena9@gmail.com>
2026-03-18 14:14:15 +0530

    Import routeIntersectsZones utility (updated version)

--- Controller/safeRouteController.js
+++ Controller/safeRouteController.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const circleToPolygon = require("../utils/circlePolygon");
+const routeIntersectsZones = require("../utils/zoneChecker");
 
 const getSafeRoute = async (req, res) => {};
 module.exports = { getSafeRoute };
